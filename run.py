9ed67596d7470285ac9a5af3450424303e47eb73
Tommy-83 <tommy.kosakowski@gmail.com>
2023-05-10 11:45:30 +0000

    tidying up the code

--- run.py
+++ run.py
@@ -3,7 +3,8 @@ from termcolor import colored
 
 def generate_word():
     words = ['apple', 'banana', 'cherry', 'grape', 'melon', 'peach', 'pear', 'plum']
-    return random.choice(words)
+    five_letter_words = [word for word in words if len(word) == 5]
+    return random.choice(five_letter_words)
 
 def check_guess(word, guess):
     if len(guess) != len(word):
@@ -32,20 +33,32 @@ def play_wordle():
     print("Welcome to Wordle!")
     print("Guess the five-letter word.")
 
-    word = generate_word()
-    attempts = 0
+    while True:
+        word = generate_word()
+        attempts = 0
+        guessed_words = []
 
-    while attempts < 6:
-        guess = input("Enter your guess: ").lower()
+        while attempts < 6:
+            guess = input("Enter your guess: ").lower()
+            guessed_words.append(guess)
 
-        if check_guess(word, guess):
-            print("Congratulations! You guessed the word.")
-            return
+            if check_guess(word, guess):
+                print("Congratulations! You guessed the word.")
+                break
 
-        attempts += 1
-        print(f"Attempt {attempts} out of 6")
+            attempts += 1
+            print(f"Attempt {attempts} out of 6")
 
-    print("Game over! You did not guess the word.")
-    print(f"The word was: {word}")
+        if attempts == 6:
+            print("Game over! You did not guess the word.")
+            print(f"The word was: {word}")
+
+        print("\nGuessed Words:")
+        for word in guessed_words:
+            print(word)
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            break
 
 play_wordle()
