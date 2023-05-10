b3dc94c9830164e10e5cb0fb044ccfcaba466a94
Tommy-83 <tommy.kosakowski@gmail.com>
2023-05-10 11:27:03 +0000

    swapping colorama to termcolor

--- run.py
+++ run.py
@@ -1,43 +1,32 @@
 import random
-import colorama
-from colorama import Fore, Style
-colorama.init()
-
-init() #Initialise colorama
+from termcolor import colored
 
 def generate_word():
-    words = [
-        'apple', 'banana', 'cherry', 'grape', 'melon', 'peach', 'pear', 'plum',
-        'table', 'chair', 'beach', 'lucky', 'prize', 'zebra', 'mouse', 'paper',
-        # ... (add more words here)
-    ]
+    words = ['apple', 'banana', 'cherry', 'grape', 'melon', 'peach', 'pear', 'plum']
     return random.choice(words)
 
 def check_guess(word, guess):
     if len(guess) != len(word):
         return False
 
-    correct_letters = []
-    misplaced_letters = []
+    feedback = []
+    correct_letters = 0
 
     for i in range(len(word)):
         if word[i] == guess[i]:
-            correct_letters.append(guess[i])
+            feedback.append(colored(word[i], 'green'))
+            correct_letters += 1
         elif guess[i] in word:
-            misplaced_letters.append(guess[i])
-
-    correct_letters_str = ''.join(correct_letters)
-    misplaced_letters_str = ''.join(misplaced_letters)
-
-    feedback = [
-        f'{Fore.GREEN}{char}{Style.RESET_ALL}' if char in correct_letters else
-        f'{Fore.YELLOW}{char}{Style.RESET_ALL}' if char in misplaced_letters else
-        f'{Fore.RED}_'
-        for char in guess
-    ]
+            feedback.append(colored(guess[i], 'yellow'))
+        else:
+            feedback.append(colored('_', 'red'))
 
     print(' '.join(feedback))
-    return guess == word
+
+    if correct_letters == len(word):
+        return True
+
+    return False
 
 def play_wordle():
     print("Welcome to Wordle!")
